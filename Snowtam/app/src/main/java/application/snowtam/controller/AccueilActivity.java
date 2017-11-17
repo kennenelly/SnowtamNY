c16c09867f8e3355c3a88630dbeb90ae41fd08b3
kennenelly <kenne_nelly@yahoo.fr>
2017-11-17 09:49:49 +0100

    firts commit

---
+++ Snowtam/app/src/main/java/application/snowtam/controller/AccueilActivity.java
@@ -0,0 +1,15 @@
+package application.snowtam.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import application.snowtam.R;
+
+public class AccueilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accuil);
+    }
+}
---
+++ Snowtam/app/src/main/java/application/snowtam/model/terrain.java
@@ -0,0 +1,8 @@
+package application.snowtam.model;
+
+/**
+ * Created by kenne on 15/11/2017.
+ */
+
+public class terrain {
+}
