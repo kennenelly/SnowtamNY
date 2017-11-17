42513c64f30428bd0583d7d648a2bc7623e572c9
kennenelly <kenne_nelly@yahoo.fr>
2017-11-17 16:45:37 +0100

    second commit

--- Snowtam/app/src/main/java/application/snowtam/controller/SnowtamActivity.java
+++ Snowtam/app/src/main/java/application/snowtam/controller/SnowtamActivity.java
@@ -115,7 +115,7 @@ public class SnowtamActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_snowtam, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_snowtamDecode, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
---
+++ Snowtam/app/src/main/java/application/snowtam/controller/SnowtamDecodeActivity.java
@@ -0,0 +1,8 @@
+package application.snowtam.controller;
+
+/**
+ * Created by kenne on 17/11/2017.
+ */
+
+public class SnowtamDecodeActivity {
+}
